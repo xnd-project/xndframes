d99c7795166c21c015f84dde9c505858e6c20c00
Anderson Banihirwe <axbanihirwe@ualr.edu>
2018-07-24 11:19:38 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,11 @@
 from setuptools import setup, find_packages
 import versioneer
 
-with open("README.md") as readme_file:
+with open("README.md", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
 
-requirements = ["pandas>=0.23.0", "gumath", "xnd"]
+requirements = ["pandas>=0.23.0", "xnd"]
 
 setup_requirements = ["pytest-runner"]
 
@@ -31,7 +29,8 @@ setup(
     description="Pandas ExtensionDType/Array backed by xnd",
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + "\n\n" + history,
+    long_description=readme,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     keywords="xndframes",
     name="xndframes",
